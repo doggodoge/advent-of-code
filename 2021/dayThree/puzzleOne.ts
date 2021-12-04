299c1ec1162905ab15ccd7626661bcb53447b6bd
Gary Moore <madmangaz@gmail.com>
2021-12-04 23:26:38 +0000

    solve 2021 day three puzzle one

--- 2021/dayThree/puzzleOne.ts
+++ 2021/dayThree/puzzleOne.ts
@@ -1,25 +1,39 @@
 import getData from './utils/getData.ts';
 
-const data = getData('data/sample.txt');
+const data = getData('data/data.txt');
 
 function getGammaRate(data: string[]): number {
   let gamma = '';
-  for (let i = 0; i < data[0].length; i++) {
-    const totalOnes = data
-      .map(item => [...item][i])
-      .map(item => Number(item))
-      .reduce((a, b) => a + b);
-    totalOnes > 500 ? (gamma += '1') : (gamma += '0');
+  for (let index = 0; index < data[0].length; index++) {
+    const numberOfOnes = data
+      .map(item => [...item][index])
+      .map(item => Number.parseInt(item, 2))
+      .reduce((first, second) => first + second);
+    const numberOfZeroes = data.length - numberOfOnes;
+    numberOfOnes > numberOfZeroes ? (gamma += 1) : (gamma += 0);
   }
   return Number.parseInt(gamma, 2);
 }
 
 function getEpsilonRate(data: string[]): number {
-  return Math.abs(~(getGammaRate(data) >>> 0));
+  let epsilon = '';
+  for (let index = 0; index < data[0].length; index++) {
+    const numberOfOnes = data
+      .map(item => [...item][index])
+      .map(item => Number.parseInt(item, 2))
+      .reduce((first, second) => first + second);
+    const numberOfZeroes = data.length - numberOfOnes;
+    numberOfOnes < numberOfZeroes ? (epsilon += 1) : (epsilon += 0);
+  }
+  return Number.parseInt(epsilon, 2);
 }
 
-const gammaRate = getGammaRate(data);
-const epsilonRate = getEpsilonRate(data);
-console.log(`gamma rate: ${gammaRate}`);
-console.log(`epsilon rate: ${epsilonRate}`);
-console.log(`power level: ${gammaRate * epsilonRate}`);
+function run() {
+  const gammaRate = getGammaRate(data);
+  const epsilonRate = getEpsilonRate(data);
+  console.log(`gamma rate: ${gammaRate}`);
+  console.log(`epsilon rate: ${epsilonRate}`);
+  console.log(`power level: ${gammaRate * epsilonRate}`);
+}
+
+run();

84836cc22667dd42e12e702c2f35e78b09848463
Gary Moore <madmangaz@gmail.com>
2021-12-18 20:00:34 +0000

    add TODO for createBoard

--- 2021/dayFive/utils/createBoard.ts
+++ 2021/dayFive/utils/createBoard.ts
@@ -1,7 +1,8 @@
 import Board from '../types/Board.ts';
 
-// Why do I have to do this terrible stuff just to create
-// a multidimensional array in JavaScript? Hideous.
+// TODO: Figure out an alternative to this hideous workaround. Array(x).fill()
+//  doesn't work because for whatever reason it uses the same row reference for
+//  all rows.
 function createBoard(x: number, y: number): Board {
   const board: Board = Array(y);
   for (let i = 0; i <= x; i++) {

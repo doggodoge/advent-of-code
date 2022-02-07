d4227d218868311d9c8936ffdf8080509c1b993b
Gary Moore <madmangaz@gmail.com>
2022-02-07 02:53:35 +0000

    reformat wonky looking code

--- 2021/dayOne/printAnswers.ts
+++ 2021/dayOne/printAnswers.ts
@@ -2,14 +2,14 @@ import getData from './utils/getData.ts';
 import puzzleOne from './puzzleOne.ts';
 import puzzleTwo from './puzzleTwo.ts';
 
-const readPermission = { name: 'read', path: './data/data.txt'} as const;
+const readPermission = { name: 'read', path: './data/data.txt' } as const;
 const permissionStatus = await Deno.permissions.request(readPermission);
 
 if (permissionStatus.state === 'granted') {
-const data = getData();
+  const data = getData();
 
-console.log(`Puzzle one: ${puzzleOne(data)}`);
-console.log(`Puzzle two: ${puzzleTwo(data)}`)
+  console.log(`Puzzle one: ${puzzleOne(data)}`);
+  console.log(`Puzzle two: ${puzzleTwo(data)}`);
 } else {
   console.error('Please allow read permissions.');
 }
